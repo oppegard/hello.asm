62c9d92bdd014fd012b35561f4129706ba8c4de9
Glenn Oppegard <oppegard@gmail.com>
2013-11-16 13:05:42 -0700

    assert_result helper

--- test.sh
+++ test.sh
@@ -8,8 +8,28 @@ function red {
   echo -e "$(tput setaf 1)$1$(tput sgr0)"
 }
 
+function assert_result () {
+  if [ -z "$1" ]; then
+    red "  FAIL: No value given to assert"
+  elif [ "${1}" == 0 ]; then
+    green "  PASS"
+  else
+    red "  FAIL: expected 0, actual ${1}"
+  fi
+}
+
 function test_nasm () {
-  nasm -f elf hello.asm
+  echo "test_nasm:"
+
+  arch=$(/usr/bin/arch)
+  if [ "${arch}" == "x86_64" ]; then
+    elf_arch="elf64"
+  else
+    elf_arch="elf32"
+  fi
+
+  nasm -f "${elf_arch}" hello.asm
+  assert_result $?
 }
 
 function cleanup () {
@@ -18,11 +38,5 @@ function cleanup () {
 
 
 test_nasm
-if [ $? == 0 ]
-  then
-  green "test_nasm passed"
-else
-  red "test_nasm failed"
-fi
 
 cleanup
